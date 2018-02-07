4942ef6d84591f2594611246d446741e3254ad1c
z <1844927304@qq.com>
2018-02-07 13:44:39 +0800

    move the controller folder to yuzhaoLiu.project

--- src/main/java/yuzhaoLiu/project/controller/testController.java
+++ src/main/java/yuzhaoLiu/project/controller/testController.java
@@ -1,4 +1,4 @@
-package yuzhaoLiu.project.mybatis.controller;
+package yuzhaoLiu.project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
